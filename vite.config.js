60fa342167d39bfef48e486a8d50182dedc02833
unaakk <1446558794@qq.com>
2025-09-05 05:03:03 +0800

    添加别名，引入less样式

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  //添加别名
+  resolve:{
+    alias:[
+      {
+        find: "@",
+        replacement: "/src",
+      }
+    ]
+  }
 })
