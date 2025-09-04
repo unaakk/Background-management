5036b7a5a9d96e34ec182a60737d84257fee7b97
unaakk <1446558794@qq.com>
2025-09-05 05:22:40 +0800

    引入pinia

--- src/main.js
+++ src/main.js
@@ -2,13 +2,16 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import "@/assets/less/index.less";
 import router from "./router";
-// import ElementPlus from 'element-plus';//全局引入
-// import 'element-plus/dist/index.css';
+// import ElementPlus from 'element-plus';//全局引入ElementPlus
+// import 'element-plus/dist/index.css';//全局引入ElementPlus
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+import { createPinia } from 'pinia';
 
 const app = createApp(App);
+const pinia = createPinia();
 
-// app.use(ElementPlus);
+app.use(pinia);
+// app.use(ElementPlus);//全局引入ElementPlus
 app.use(router).mount("#app"); 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
